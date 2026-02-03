18149de7e4bd96e92817d834affe89cb22223ce0
ak8488644-design <ak8488644@gmail.com>
2026-02-03 19:51:46 +0530

    Delete Exp3/exp3.1/App.jsx

--- Exp3/exp3.1/App.jsx
+++
@@ -1,9 +0,0 @@
-function ProductCard(props) {
-  return (
-    <div className="card">
-      <h3>{props.name}</h3>
-      <p>₹{props.price}</p>
-      <p>{props.inStock ? "In Stock" : "Out of Stock"}</p>
-    </div>
-  );
-}
