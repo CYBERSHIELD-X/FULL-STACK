06c4438340ef1b8f7deca2588b95ee33faddd17f
ak8488644-design <ak8488644@gmail.com>
2026-02-26 23:03:12 +0530

    Create App.js

---
+++ Exp7/exp7.1/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "./ProductList";
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-center mt-3">Product Catalog</h2>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
