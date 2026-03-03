c024657d8389e52b9a9dc03ad31cf354dbf51bbd
ak8488644-design <ak8488644@gmail.com>
2026-03-03 15:40:17 +0530

    Delete Exp6/exp6.3 directory

--- Exp6/exp6.3/models/Account.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const accountSchema = new mongoose.Schema({
-  name: String,
-  balance: Number
-});
-
-module.exports = mongoose.model("Account", accountSchema);
