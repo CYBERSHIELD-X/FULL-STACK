40eb883f9f56f9c12b08c3dc44bad65a7c20370c
ak8488644-design <ak8488644@gmail.com>
2026-03-25 18:00:29 +0530

    Delete Exp9/exp9.3 directory

--- Exp9/exp9.3/optimize-app/src/HeavyComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function HeavyComponent({ count }) {
-  console.log("Heavy Component Rendered");
-
-  return <h3>Heavy Count: {count}</h3>;
-}
-
-export default HeavyComponent;
