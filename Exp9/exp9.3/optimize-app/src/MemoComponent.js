594bdf4d6538684ca0490801675eac4d394640fb
ak8488644-design <ak8488644@gmail.com>
2026-03-25 13:10:37 +0530

    Delete Exp9/exp9.3/optimize-app/src/MemoComponent.js

--- Exp9/exp9.3/optimize-app/src/MemoComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MemoComponent = React.memo(({ value }) => {
-  console.log("Memo Component Rendered");
-  return <h3>Memo Value: {value}</h3>;
-});
-
-export default MemoComponent;
