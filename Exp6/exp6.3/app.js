195b489aff0550747bc63654df06b228a250eb66
ak8488644-design <ak8488644@gmail.com>
2026-04-01 11:57:22 +0530

    Update app.js

--- Exp6/exp6.3/app.js
+++ Exp6/exp6.3/app.js
@@ -10,8 +10,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/ecommerceDB')
   .catch(err => console.log(err));
 
 // Routes
-const productRoutes = require('./routes/productRoutes');
-app.use('/products', productRoutes);
+const transactionRoutes = require('./routes/transactionRoutes');
+app.use('/', transactionRoutes);
 
 // Server
 app.listen(3000, () => {
